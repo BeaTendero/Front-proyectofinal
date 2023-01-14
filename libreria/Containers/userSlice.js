e7d38fef6f4a778ab76cdff9995a71edf8792d40
Bea Tendero <beatriz.tendero.hernandez@gmail.com>
2023-01-14 17:54:20 +0100

    guardado de emergencia probando arreglar errrores

---
+++ libreria/Containers/userSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+//Método que me permite crear el slice
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+      credentials: {}
+    },
+    reducers: {
+      login: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      },
+      userout: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      }
+      
+    }
+    
+});
+
+export const { login, userout } = userSlice.actions;//Este login que exportamos es el mismo login que contiene los reducers.
+//Lo llamaremos en el componente container login
+
+//userData contiene el estado del reducer, es decir, userData es lo que van a leer
+// los componentes conectados a este reducer, para saber sus credenciales
+
+export const userData = (state) => state.user;
+  
+export default userSlice.reducer;
--- libreria/src/App.js
+++ libreria/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import express from "express"
 import { Button } from 'reactstrap';
+
+const app = express()
+const port = 3306
+
+app.use(express.json({limit:"100mbcl"}))
+
+app.post("/api/users",(req,res)=>{
+  console.log("dummy endPoint")
+  res.send("You have posted something")
+})
+
+app.get("/",(req,res)=> {
+  res.send("Hello word")
+})
+
 
 function App() {
   return (
